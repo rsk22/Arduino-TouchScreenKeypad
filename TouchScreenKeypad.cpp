f5c88316f8602a2bd91f88feb800ce26bb19ee71
Richard Kirkpatrick <rskirkpatrick22@gmail.com>
2014-07-17 18:59:41 -0500

    First commit
    Library for drawing a keypad to the Seeed Studio TFT Touch Screen

---
+++ TouchScreenKeypad.cpp
@@ -0,0 +1,316 @@
+/*
+  TouchScreenKeypad.h - Library for creating the touch screen keypad for the Seeed Studio TFT Touch Screen (Version 1).
+  21 June 2014: Created by Richard Kirkpatrick
+*/
+#include "Arduino.h"
+#include "TouchScreenKeypad.h"
+
+#include <stdint.h>
+#include <TouchScreenGeometry.h>
+#include <TouchScreenStrings.h>
+#include <TouchScreenButtons.h>
+
+#include <TouchScreen.h>
+#include <TFT.h>
+#include <math.h>
+
+/*
+----------------------------------------------------------------
+                   TouchScreenText Superclass
+----------------------------------------------------------------
+*/
+
+TouchScreenText::TouchScreenText() : xstart(0), ystart(0), fontSize(1), textColor(0xffff)
+{
+
+}
+
+TouchScreenText::TouchScreenText(TouchScreenText& otherTouchScreenText)
+{
+   xstart = otherTouchScreenText.xstart;
+   ystart = otherTouchScreenText.ystart;
+   fontSize = otherTouchScreenText.fontSize;
+   textColor = otherTouchScreenText.textColor;
+}
+
+
+TouchScreenText::TouchScreenText(int myXStart, int myYStart, int myFontSize, unsigned int myTextColor)
+    : xstart(myXStart), ystart(myYStart), fontSize(myFontSize), textColor(myTextColor)
+{
+
+}
+
+
+void TouchScreenText::setTextCoord(int myXStart, int myYStart)
+{
+   xstart = myXStart;
+   ystart = myYStart;
+}
+
+
+void TouchScreenText::setFontSize(int myFontSize)
+{
+   fontSize = myFontSize;
+}
+
+
+void TouchScreenText::setTextColor(unsigned int myTextColor)
+{
+   textColor = myTextColor;
+}
+
+
+const int TouchScreenText::getXStart()
+{
+   return xstart;
+}
+
+
+const int TouchScreenText::getYStart()
+{
+   return ystart;
+}
+
+
+const int TouchScreenText::getFontSize()
+{
+   return fontSize;
+}
+
+
+const int TouchScreenText::getTextColor()
+{
+   return textColor;
+}
+
+
+/*
+----------------------------------------------------------------
+		Functions for the TouchScreenString Subclass
+----------------------------------------------------------------
+*/
+
+TouchScreenString::TouchScreenString() : TouchScreenText(), text("")
+{
+
+}
+
+
+TouchScreenString::TouchScreenString(TouchScreenString& otherTouchScreenString)
+{
+  text = otherTouchScreenString.text;
+  xstart = otherTouchScreenString.xstart;
+  ystart = otherTouchScreenString.ystart;
+  fontSize = otherTouchScreenString.fontSize;
+  textColor = otherTouchScreenString.textColor;
+}
+
+
+TouchScreenString::TouchScreenString(char* myText, int myXStart, int myYStart, int myFontSize, unsigned int myTextColor)
+    : text(myText), TouchScreenText(myXStart, myYStart, myFontSize, myTextColor)
+{
+
+}
+
+
+void TouchScreenString::setValues(char* myText, int myXStart, int myYStart, int myFontSize, unsigned int myTextColor)
+{
+   text = myText;
+   xstart = myXStart;
+   ystart = myYStart;
+   fontSize = myFontSize;
+   textColor = myTextColor;
+}
+
+
+void TouchScreenString::setText(char* myText)
+{
+   text = myText;
+}
+
+
+const char* TouchScreenString::getText()
+{
+   return text;
+}
+
+
+void TouchScreenString::drawText()
+{
+   Tft.drawString(text, xstart, ystart, fontSize, textColor);
+}
+
+
+void TouchScreenString::textButtonDisplay()
+{
+   setTextColor(0xf800); ///< Sets the color to red
+   drawText();
+   delay(100);
+   setTextColor(0xffff); ///< Sets the color to white
+   drawText();
+}
+
+/*
+----------------------------------------------------------------
+		Functions for the TouchScreenChar Subclass
+----------------------------------------------------------------
+*/
+
+TouchScreenChar::TouchScreenChar() : TouchScreenText(), text('0')
+{
+
+}
+
+
+TouchScreenChar::TouchScreenChar(TouchScreenChar& otherTouchScreenChar)
+{
+  text = otherTouchScreenChar.text;
+  xstart = otherTouchScreenChar.xstart;
+  ystart = otherTouchScreenChar.ystart;
+  fontSize = otherTouchScreenChar.fontSize;
+  textColor = otherTouchScreenChar.textColor;
+}
+
+
+TouchScreenChar::TouchScreenChar(char myText, int myXStart, int myYStart, int myFontSize, unsigned int myTextColor)
+    : text(myText), TouchScreenText(myXStart, myYStart, myFontSize, myTextColor)
+{
+
+}
+
+
+void TouchScreenChar::setValues(char myText, int myXStart, int myYStart, int myFontSize, unsigned int myTextColor)
+{
+   text = myText;
+   xstart = myXStart;
+   ystart = myYStart;
+   fontSize = myFontSize;
+   textColor = myTextColor;
+}
+
+
+void TouchScreenChar::setText(char myText)
+{
+   text = myText;
+}
+
+
+const char TouchScreenChar::getText()
+{
+   return text;
+}
+
+
+void TouchScreenChar::drawText()
+{
+   Tft.drawChar(text, xstart, ystart, fontSize, textColor);
+}
+
+
+void TouchScreenChar::textButtonDisplay()
+{
+   setTextColor(0xf800); ///< Sets the color to red
+   drawText();
+   delay(100);
+   setTextColor(0xffff); ///< Sets the color to white
+   drawText();
+}
+
+/*
+----------------------------------------------------------------
+                        TouchScreenKeypad
+----------------------------------------------------------------
+*/
+
+TouchScreenKeypad::TouchScreenKeypad() : xCursor(20)
+{
+}
+
+void TouchScreenKeypad::drawKeypad()
+{
+
+    const int xstartButton[] = {15, 95, 175};           // x-min for keypads
+    const int ystartButton[] = {65, 127, 189, 251};     // y-min for keypads
+    const int xendButton[] = {65, 145, 225};            // x-max for keypads
+    const int yendButton[] = {115, 177, 239, 301};      // y-min for keypads
+    const int xstartButtonText[] = {35, 115, 195};      // x-coordinate for keypad numbers
+    const int ystartButtonText[] = {85, 145, 209, 271}; // y-coordinate for keypad numbers
+    const int widthButton = 45;
+    const int heightButton = widthButton;
+    char textKeypad[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'C', '0', 'E'}; // Text for the buttons
+
+    // Sets the xstart, ystart, width, and height of the textblock and then draws it
+    Rectangle textBlock(15, 15, 216, 35, WHITE, BLACK);
+    textBlock.draw();
+
+    // Sets the coordinates and sizes of the keypad buttons, and sets all the values for the buttons' text
+    const int noRows = 4;
+    const int noColumns = 3;
+    for(int y = 0; y < noRows; y++) {
+        for(int x = 0; x < noColumns; x++) {
+            int buttonNumber = noColumns * y + x;
+            button[buttonNumber].setValues(xstartButton[x], ystartButton[y], widthButton, heightButton);
+            buttonText[buttonNumber].setValues(textKeypad[noColumns * y + x], xstartButtonText[x], ystartButtonText[y], 1, WHITE);
+    }
+  }
+
+    // Draws the keypads and their text
+    for(int x = 0; x < 12; x++) {
+        button[x].draw();
+        buttonText[x].drawText();
+  }
+}
+
+void TouchScreenKeypad::displayButton(const int userInput)
+{
+  // Only allow valid buttons to be displayed to prevent errors
+  if (userInput >= 0 && userInput <= 12) {
+      button[userInput].buttonDisplay();
+      buttonText[userInput].textButtonDisplay();
+  }
+}
+
+int TouchScreenKeypad::getButton(int xInput, int yInput)
+{
+    for (int i = 0; i < 12; i++) {
+        if ((xInput > button[i].getXStart() && xInput < button[i].getXEnd())
+            && (yInput > button[i].getYStart() && yInput < button[i].getYEnd()))
+                return i;
+    }
+
+    // If no button is presed, return -1 signifying no button was pressed
+    return -1;
+}
+
+
+void TouchScreenKeypad::displayText(char buttonText)
+{
+  // Ensures text input is not beyond width of screen and only a valid button.
+  if (xCursor > 200) {
+    // If true, it resets the input and notifies user.
+    clearTextBlock();
+    Tft.drawString("Too many inputs", 65, 30, 1, WHITE);
+    delay(2000);
+    clearTextBlock();
+  } else {
+    Tft.drawChar(buttonText, xCursor, 25, 2, WHITE);
+    xCursor += 20;
+  }
+}
+
+void TouchScreenKeypad::clearTextBlock()
+{
+    Tft.fillRectangle(16, 16, 214, 33, 0x0000);
+    delay(10);
+    xCursor = 20;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ TouchScreenKeypad.h
@@ -0,0 +1,278 @@
+/*
+  TouchScreenKeypad.h - Library for creating the touch screen keypad for the Seeed Studio TFT Touch Screen (Version 1).
+  21 June 2014: Created by Richard Kirkpatrick
+*/
+
+#ifndef TouchScreenKeypad_h
+#define TouchScreenKeypad_h
+
+#include "Arduino.h"
+#include <TouchScreenGeometry.h>
+#include <TouchScreenStrings.h>
+#include <TouchScreenButtons.h>
+
+/*
+----------------------------------------------------------------
+				  TouchScreenText Superclass
+----------------------------------------------------------------
+*/
+
+///@class TouchScreenText
+///@brief Base class for drawing text to the touch screen.
+class TouchScreenText {
+public:
+
+    /*!
+    @brief Default constructor for the TouchScreenText superclass
+    @param  xstart The x-coordinate for the text instance.
+    @param  ystart The y-coordinate for the text instance.
+    @param  fontSize The font size of the text instance.
+    @param  textColor The color of the text instance. Default color is WHITE.
+    */
+    TouchScreenText();
+
+    /*!
+    @class TouchScreenText
+    @brief Parameter constructor for the TouchScreenText superclass
+    @param  otherTouchScreenText The text that is to be copied.
+    */
+    TouchScreenText(TouchScreenText& otherTouchScreenText);
+
+
+    /*!
+    @brief Parameter constructor for the TouchScreenText superclass
+    @param  myXStart The x-coordinate for the text instance.
+    @param  myYStart The y-coordinate for the text instance.
+    @param  myFontSize The font size of the text instance.
+    @param  myTextColor The color of the text instance. Default color is WHITE.
+    */
+
+    TouchScreenText(int, int, int, unsigned int);
+
+    /*!
+    @brief Sets the coordinates of the text.
+    @param  myXStart The x-coordinate for the text instance.
+    @param  myYStart The y-coordinate for the text instance.
+    */
+
+    void setTextCoord(int, int);
+
+    /*!
+    @brief Sets the font size of the text.
+    @param  myFontSize The font size of the text instance.
+    */
+    void setFontSize(int defaultFontText = 2);
+
+    /*!
+    @brief Sets the color of the text.
+    @param  myTextColor The color of the text instance.
+    */
+    void setTextColor(unsigned int textColor = 0xffff);  // Default color is WHITE.
+
+    /*!
+    @brief Gets the xcoordinate of the text.
+    @return The x-coordinate for the text instance.
+    */
+    const int getXStart();
+
+    /*!
+    @brief Gets the ycoordinate of the text.
+    @return The y-coordinate for the text instance.
+    */
+    const int getYStart();
+
+    /*!
+    @brief Gets the font size of the text.
+    @return The font size for the text instance.
+    */
+    const int getFontSize();
+
+    /*!
+    @brief Gets the font color of the text.
+    @return The font color for the text instance.
+    */
+    const int getTextColor();
+
+protected:
+  int xstart, ystart; ///< Coordinates of the text
+  int fontSize; ///< Size of the text
+  unsigned int textColor; ///< Color of the text
+};
+
+/*
+----------------------------------------------------------------
+				  TouchScreenString Subclass
+----------------------------------------------------------------
+*/
+
+/*!
+@class TouchScreenString
+@brief Subclass of TouchScreenText. Abstract class for drawing strings to the touch screen.
+*/
+class TouchScreenString : public TouchScreenText {
+ public:
+    /*!
+    @brief Default constructor for the TouchScreenString subclass.
+    @param  text The text that is to be drawn.
+    */
+    TouchScreenString();
+
+    /*!
+    @brief Copy constructor for the TouchScreenString superclass
+    @param  otherTouchScreenString The string instance that is to be copied.
+    */
+    TouchScreenString(TouchScreenString& otherTouchScreenString);
+
+    /*!
+    @brief Parameter constructor for the TouchScreenString superclass.
+    @param  myText The text that is to be drawn.
+    @param  myXStart The x-coordinate for the text instance.
+    @param  myYStart The y-coordinate for the text instance.
+    @param  myFontSize The font size of the text instance.
+    @param  myTextColor The color of the text instance.
+    */
+    TouchScreenString(char* myText, int myXStart, int myYStart, int myFontSize, unsigned int myTextColor);
+
+    /*!
+    @brief Sets the text, coordinates, font size, and text color of the instance.
+    @param  myText The text that is to be drawn.
+    @param  myXStart The x-coordinate for the text instance.
+    @param  myYStart The y-coordinate for the text instance.
+    @param  myFontSize The font size of the text instance.
+    @param  myTextColor The color of the text instance. Default color is WHITE.
+    */
+    void setValues(char* myText, int myXStart, int myYStart, int myFontSize, unsigned int myTextColor);
+
+    /*!
+    @brief Sets the text of the string instance.
+    @param  myText The text that is to be drawn.
+    */
+    void setText(char*);
+
+    /*!
+    @brief Returns the text of the string instace.
+    @return text The text that is to be drawn.
+    */
+    const char* getText();
+
+    /*!
+    @brief Uses the TFT library to draw the string instance.
+    */
+    void drawText();
+
+    /*!
+    @brief Highlights the button text when pressed.
+    */
+    void textButtonDisplay();
+
+ private:
+    char* text;
+};
+
+/*
+----------------------------------------------------------------
+				  TouchScreenChar Subclass
+----------------------------------------------------------------
+*/
+
+///@class TouchScreenChar
+///@brief Subclass of TouchScreenText.  Abstract class used for drawing char's to the touch screen.
+class TouchScreenChar : public TouchScreenText {
+ public:
+    /*!
+    @brief Default constructor for the TouchScreenString subclass.
+    */
+    TouchScreenChar();
+
+    /*!
+    @brief Copy constructor for the TouchScreenString subclass.
+    @param  otherTouchScreenChar The char instance that is being copied.
+    */
+    TouchScreenChar(TouchScreenChar& otherTouchScreenChar);
+
+    /*!
+    @brief Parameter constructor for the TouchScreenString superclass.
+    @param  myText The char that is to be drawn.
+    @param  myXStart The x-coordinate for the char instance.
+    @param  myYStart The y-coordinate for the char instance.
+    @param  myFontSize The font size of the char instance.
+    @param  myTextColor The color of the char instance.
+    */
+    TouchScreenChar(char myText, int myXStart, int myYStart, int myFontSize, unsigned int myTextColor);
+
+    /*!
+    @brief Sets the char, cooordinates, font size, and char color.
+    @param  myText The char that is to be drawn.
+    @param  myXStart The x-coordinate for the char instance.
+    @param  myYStart The y-coordinate for the char instance.
+    @param  myFontSize The font size of the char instance.
+    @param  myTextColor The color of the char instance.
+    */
+    void setValues(char myText, int myXStart, int myYStart, int myFontSize, unsigned int myTextColor);
+
+    /*!
+    @brief Sets the text of the char instance.
+    @param  myText The char that is to be drawn.
+    */
+    void setText(char myText);
+
+    /*!
+    @brief Returns the char of the char instance.
+    @return The char that is to be drawn.
+    */
+    const char getText();
+
+    ///@brief Uses the Seeed Studio library to draw the char.
+    void drawText();
+
+    ///@brief Highlights the button text when pressed.
+    void textButtonDisplay();
+
+ private:
+    char text;
+};
+
+/*
+----------------------------------------------------------------
+                        TouchScreenKeypad
+----------------------------------------------------------------
+*/
+
+///@class TouchScreenKeypad
+///@brief Class for handling all the TFT Touch Screen keypad functions
+class TouchScreenKeypad {
+ public:
+    ///@brief Default constructor for the Keypad class
+    TouchScreenKeypad();
+
+    ///@brief Draws the keypad to the scree
+    void drawKeypad();
+
+    ///@brief Highlights the button when pressed
+    ///@param  userInput    The button that was selected by the user.
+    void displayButton(const int userInput);
+
+    /**
+    @brief Gets the button number that was pressed
+    @param  xInput   The x-coordinate of the user's input.
+    @param  yInput   The y-coordinate of the user's input.
+    */
+    int getButton(int xInput, int yInput);
+
+    /**
+    @brief Displays the text to the screen
+    @param  buttonText  The text for the button
+    */
+    void displayText(char buttonText);
+
+    ///@brief Clears the textblock and resets the cursor to the original position.
+    void clearTextBlock();
+
+ private:
+    Button button[12];
+    TouchScreenChar buttonText[12];
+    Rectangle textBlock;
+    int xCursor; ///< Used for positioning the text on the screen
+};
+
+#endif
